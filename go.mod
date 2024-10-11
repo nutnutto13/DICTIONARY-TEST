03f458d0dbc3b7b22fdd88829b10ac74b9e2fa7d
nutnutto13 <tanaporn.nutto@gmail.com>
2024-10-12 00:04:46 +0700

    init

---
+++ go.mod
@@ -0,0 +1,26 @@
+module dictionary-test
+
+go 1.23.2
+
+require github.com/line/line-bot-sdk-go/v7 v7.21.0
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/karrick/godirwalk v1.12.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/ramya-rao-a/go-outline v0.0.0-20210608161538-9736a4bde949 // indirect
+	github.com/uudashr/gopkgs/v2 v2.1.2 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221212164502-fae10dda9338 // indirect
+	golang.org/x/mod v0.20.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/telemetry v0.0.0-20240829154258-f29ab539cc98 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/tools v0.22.1-0.20240829175637-39126e24d653 // indirect
+	golang.org/x/tools/gopls v0.16.2 // indirect
+	golang.org/x/vuln v1.0.4 // indirect
+	honnef.co/go/tools v0.4.7 // indirect
+	mvdan.cc/gofumpt v0.6.0 // indirect
+	mvdan.cc/xurls/v2 v2.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+func main() {
+	channelSecret := "f8830b25d8c4f293448712ddf9c5cc12"
+	channelAccessToken := "e0OFDDYPrDJZE4ZcUAJVAmIGxRy+ZhcYiSA9Yqe9Uk+M7dFRF4lKOtjDtTII/6r78hqlumtw/Z5zolTOf4589mXpdkTyqldbOntJrWOpe4SlVh44veIihUGGQvIpcfHV9D0HeXF7UTQfPIi1UqGw6QdB04t89/1O/w1cDnyilFU="
+	log.Print("start......")
+	bot, err := linebot.New(channelSecret, channelAccessToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//webhook
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		events, err := bot.ParseRequest(r)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(http.StatusBadRequest)
+			}
+			return
+		}
+
+		//for loop suport word has multiple meanings
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				if message, ok := event.Message.(*linebot.TextMessage); ok {
+
+					var newTextMessages []linebot.SendingMessage
+					result, err := GetDictionary(message.Text)
+					if err != nil {
+						newTextMessages = append(newTextMessages, linebot.NewTextMessage("Sorry : "+err.Error()))
+					} else {
+						for i, item := range result {
+							newTextMessages = append(newTextMessages, linebot.NewTextMessage("Meanings"+strconv.Itoa(i+1)+" : "+item.Meanings[0].Definitions[0].Definition+"\nSynonyms: "+strings.Join(item.Meanings[0].Definitions[0].Synonyms, " ")))
+						}
+					}
+
+					_, err = bot.ReplyMessage(event.ReplyToken, newTextMessages...).Do()
+					if err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func GetDictionary(word string) ([]Result, error) {
+	//validate input
+	var validWord = regexp.MustCompile(`^[a-zA-Z]+$`)
+	if !validWord.MatchString(word) {
+		return []Result{}, errors.New("input invalid, pleas try again to input only English")
+	}
+
+	url := fmt.Sprintf("https://api.dictionaryapi.dev/api/v2/entries/en/%s", word)
+	resp, err := http.Get(url)
+	if err != nil {
+		return []Result{}, errors.New("http get error")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return []Result{}, errors.New("http get error")
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []Result{}, errors.New("http get error")
+	}
+
+	//unmarshal dictionary output to model
+	var result []Result
+	err = json.Unmarshal([]byte(data), &result)
+	if err != nil {
+		return []Result{}, errors.New("unmarshal error")
+	}
+
+	return result, nil
+
+}
+
+// Model
+type Definition struct {
+	Definition string   `json:"definition"`
+	Synonyms   []string `json:"synonyms"`
+	Antonyms   []string `json:"antonyms"`
+}
+
+type Meaning struct {
+	PartOfSpeech string       `json:"partOfSpeech"`
+	Definitions  []Definition `json:"definitions"`
+	Synonyms     []string     `json:"synonyms"`
+	Antonyms     []string     `json:"antonyms"`
+}
+
+type Result struct {
+	Word     string    `json:"word"`
+	Meanings []Meaning `json:"meanings"`
+}
